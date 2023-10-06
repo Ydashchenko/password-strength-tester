4c1784a91d2cc3fdd8edbc9de752943a6b9a8565
madjonga <madjonga123@gmail.com>
2023-10-06 19:29:00 +0300

    Finish the task

---
+++ src/app/password-strength/password-strength.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-strength',
+  templateUrl: './password-strength.component.html',
+  styleUrls: ['./password-strength.component.css']
+})
+export class PasswordStrengthComponent {
+  password: string = '';
+  passwordStrength: string = '';
+
+  checkPasswordStrength() {
+    if (this.password.length === 0) {
+      this.passwordStrength = 'Empty';
+    } else if (this.password.length < 8) {
+      this.passwordStrength = 'Short';
+    } else {
+      const hasLetters = /[a-zA-Z]/.test(this.password);
+      const hasDigits = /\d/.test(this.password);
+      const hasSymbols = /[!@#$%^&*,.]/.test(this.password);
+
+      if (hasLetters && hasDigits && hasSymbols) {
+        this.passwordStrength = 'Strong';
+      } else if ((hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols)) {
+        this.passwordStrength = 'Medium';
+      } else {
+        this.passwordStrength = 'Weak';
+      }
+    }
+  }
+}
